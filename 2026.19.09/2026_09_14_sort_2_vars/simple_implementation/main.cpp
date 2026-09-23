e50435d704bc04c707d8682b93695c452abdf0b1
SergeyLaskin <laskin2022@mail.ru>
2026-09-23 15:20:56 +0300

    Исправил вывод

--- 2026.19.09/2026_09_14_sort_2_vars/simple_implementation/main.cpp
+++ 2026.19.09/2026_09_14_sort_2_vars/simple_implementation/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 void my_sort(int *arr, const int size);
+void print_array(const char message[], int* arr, const int size);
 
 // TODO Интерфейс пользователя должен быть на русском языке
 int main() {
@@ -17,27 +18,30 @@ int main() {
 		std::cin >> arr[i];
 	}
 
-	std::cout << "Массив до сортировки: ";
-
-	for (int i = 0; i < size; i++) {
-		std::cout << arr[i] << " ";
-	}
+	
+	print_array("Массив до сортировки", arr, size);
+	
 
 	std::cout << std::endl;
 	// TODO вызвается void my_sort(int *arr, int size)
 	my_sort(arr, size);
 	// TODO Выводится первоначальный массив и отсортированный
-	std::cout << "Массив после сортировки: ";
-
-	for (int i = 0; i < size; i++) {
-
-		std::cout << arr[i] << " ";
-
-	}
+	print_array("Массив после сортировки", arr, size);
 
 	std::cout << std::endl;
 	delete[] arr;
 }
+
+void print_array(const char msg[], int* arr, const int size) {
+    std::cout << msg;
+
+    for (int i = 0; i < size; i++) {
+        std::cout << " " << arr[i];
+    }
+
+    std::cout << std::endl;
+}
+
 
 void my_sort(int *arr, const int size) {
 	// TODO
